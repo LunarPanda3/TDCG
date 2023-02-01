4bdca0cfe4b1ca0929da2df50434a6bcaff6b8f0
Jacob <jacobolsondesign@gmail.com>
2023-02-01 14:23:44 -0500

    Adding Project To Repo

---
+++ Tower Defense Card Game/Assets/Scripts/EndTurnScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTurnScript : MonoBehaviour
+{
+    public bool endTurn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endTurn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick() 
+    {
+        endTurn = true;
+        Debug.Log("Clicked!");
+    }
+}
