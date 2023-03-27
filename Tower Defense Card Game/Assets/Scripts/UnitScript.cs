ca462b02b6b67b7fed1a79dbff3d9c56bf552e44
LAPTOP-CH4NN5T5\Jacob <jacobolsondesign@gmail.com>
2023-03-27 17:22:15 -0400

    Update to Laptop Branch

--- Tower Defense Card Game/Assets/Scripts/UnitScript.cs
+++ Tower Defense Card Game/Assets/Scripts/UnitScript.cs
@@ -8,9 +8,15 @@ public class UnitScript : MonoBehaviour
     public GameObject pathNode;
     public List<GameObject> pathNodeList;
     public Transform PathingNodesHolder;
+    public Transform SplitPathHolder1;
+    public Transform SplitPathHolder2;
     GameObject nextNode;
     int nextNodeNum;
+    int nextSplitPathNodeNum;
     bool followUpdated;
+    bool atEnd = false;
+    bool onSplitPath = false;
+    int pathNum = 0;
 
     void Awake() 
     {
@@ -22,18 +28,58 @@ public class UnitScript : MonoBehaviour
     void Start()
     {
         nextNodeNum = 0;
+        nextSplitPathNodeNum = 0;
         nextNode = PathingNodesHolder.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(PathingNodesHolder.GetChild(nextNodeNum).gameObject.transform.position, transform.position) < 20f)
+        if (Vector3.Distance(PathingNodesHolder.GetChild(nextNodeNum).gameObject.transform.position, transform.position) < 20f && atEnd == false)
+        {
+            if (PathingNodesHolder.GetChild(nextNodeNum).gameObject.name == "SplitPath1" || PathingNodesHolder.GetChild(nextNodeNum).gameObject.name == "SplitPath2")
+            {
+                onSplitPath = true;
+                pathNum = Random.Range(0,2);
+            }
+            if (onSplitPath == false) 
+            {
+                nextNodeNum++;
+                if (nextNode != null)
+                {
+                    nextNode = PathingNodesHolder.GetChild(nextNodeNum).gameObject;  
+                } else 
+                {
+                    Debug.Log("At the end");
+                    atEnd = true;
+                    Destroy(this);
+                }
+            } else 
+            {
+                nextSplitPathNodeNum++;
+                if (nextNode != null)
+                {
+                    if (pathNum == 1)
+                    {
+                        nextNode = SplitPathHolder1.GetChild(nextSplitPathNodeNum).gameObject;  
+                    } else {
+                        nextNode = SplitPathHolder2.GetChild(nextSplitPathNodeNum).gameObject;  
+                    }
+                } else 
+                {
+                    Debug.Log("At the end");
+                    onSplitPath = false;
+                }
+               
+            }
+            //Debug.Log("Next Point");
+        }
+        /*if (Vector3.Distance(PathingNodesHolder.GetChild(nextNodeNum).gameObject.transform.position, transform.position) < 20f)
         {
             nextNodeNum++;
             nextNode = PathingNodesHolder.GetChild(nextNodeNum).gameObject;
             Debug.Log("Next Point");
-        }
+        }*/
 
         navMeshAgent.SetDestination(nextNode.transform.position);
     }
